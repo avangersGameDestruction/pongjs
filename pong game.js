4941c78e0596aa8dfac6252eec71f81741c7d6ea
Dutchy <63201809+avangersKinkyClient@users.noreply.github.com>
2021-11-07 15:52:08 +0100

    Update pong game.js
    create canvas

--- pong game.js
+++ pong game.js
@@ -1,2 +1,9 @@
 // create a small pong game
-function pong() {}
+function pong() {
+    // create a canvas
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    canvas.width = 400;
+    canvas.height = 400;
+    document.body.appendChild(canvas);
+}
